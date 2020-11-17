8407dfc0773745c1b78fa156c3c89dbd3cff5fec
Josh <josh.handon@gmail.com>
2020-11-17 15:13:19 -0500

    commit tree 726e6e79fc84d3f2156703f801eb49c86f5c6ef3

---
+++ reverse_each_word.rb
@@ -0,0 +1,12 @@
+def reverse_each_word(string)
+   reversed = []
+   string.split(" ").each do |word|
+   reversed << word.reverse
+   end
+   reversed.join(" ")
+ end
+
+ def reverse_each_word(string)
+    string.split.collect {|word| word.reverse}.join(" ")
+ end
+ 
